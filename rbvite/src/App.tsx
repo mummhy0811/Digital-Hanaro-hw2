97912558b4eebe24b5956ec5dd6c68515ebbb045
mummhy0811 <mummhy0811@inu.ac.kr>
2024-02-28 23:54:09 +0900

    Fix: 라우터로 변경

--- rbvite/src/App.tsx
+++ rbvite/src/App.tsx
@@ -2,15 +2,17 @@ import { useRef } from 'react';
 import './App.css'
 import { Login, LoginHandler } from './components/Login'
 import { SessionProvider } from './contexts/session-context';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   const loginHandlerRef = useRef<LoginHandler>(null);
 
   return (
     <>
-    <SessionProvider
-        loginHandlerRef={loginHandlerRef}
-      >
-      <Login/>
+      <SessionProvider loginHandlerRef={loginHandlerRef}>
+        <Routes>
+          <Route path='/' element={<Login />} />
+        </Routes>
       </SessionProvider>
     </>
   )

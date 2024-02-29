9d1a93c35486939684e42fcd4c0f75081069dc3b
mummhy0811 <mummhy0811@gmail.com>
2024-02-29 17:40:59 +0900

    Feat: 네비바에 유저 이름 출력

--- rbvite/src/Nav.tsx
+++ rbvite/src/Nav.tsx
@@ -14,12 +14,12 @@ export const Nav = () => {
   return (
     <nav>
       <div className="flex justify-around p-5 font-bold bg-cyan-500 text-white rounded-lg">
-        <h1 className="font-bold ">Hanaro Album</h1>
+        <h1 className="font-bold text-lg">Hanaro Album</h1>
 
         {loginUser?.id ? (
           <div className="flex justify-around">
             <div className="text-gray-600">{loginUser?.id}</div>
-            <div className="text-black">dfadf</div>
+            <div className="text-black mx-5">{loginUser?.username}</div>
             <NavLink
               to="/"
               className="bg-green-500 text-white rounded-md px-3"
